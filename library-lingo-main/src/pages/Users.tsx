ac3e41d319dadc10463e856ad598d7f19d77cf56
Akash Srivastava <akashpsit13@gmail.com>
2025-02-06 14:48:19 +0530

    Home Page

---
+++ library-lingo-main/src/pages/Users.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "../components/Header";
+import Index from "../pages/Index";
+import NotFound from "../pages/NotFound";
+import Admin from "../pages/Admin";
+import AdminLogin from "../pages/AdminLogin";
+
+const queryClient = new QueryClient();
+
+const Users = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default Users;
